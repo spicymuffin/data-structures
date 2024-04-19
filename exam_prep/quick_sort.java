9caf94a4a3ccb55da509c2227fc199c5cdd4870f
luigi <luigicussigh59@gmail.com>
2024-04-19 20:54:55 +0900

    library

---
+++ exam_prep/quick_sort.java
@@ -0,0 +1,15 @@
+public class quick_sort {
+    public static void sort(int[] a, int n) {
+
+    }
+
+    void quick_sort(int[] a, int n, int left, int right) {
+        int pivot = left;
+        int j = left;
+        for (int i = left + 1; i <= right; i++) {
+            if (a[i] > pivot) {
+
+            }
+        }
+    }
+}
--- exam_prep/sort_tester.java
+++ exam_prep/sort_tester.java
@@ -11,7 +11,7 @@ public class sort_tester {
     public static void main(String[] args) {
         int[] test_array = { 3, 6, 1, 2, 8, 9, 3 }; // n = 7
 
-        insertion_sort.sort(test_array, 7);
+        quick_sort.sort(test_array, 7);
 
         printarr(test_array, 7);
     }
