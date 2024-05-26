5076f398b4933a1c23def0cc90c9bfd01f637e39
spicymuffin <54279408+spicymuffin@users.noreply.github.com>
2024-05-27 02:14:15 +0900

    oops i think i wrote the heap thing wrong gg

--- assignment_04/src/Node.java
+++ assignment_04/src/Node.java
@@ -1,7 +1,7 @@
 public class Node {
     int v1_indx;    // connected vertex's index
     int weight;     // connection to the other vertex's weight
-    Node next;      // pointer to next node in LL (has nothing to do with graph)
+    Node next;      // pointer to next node in LL (has nothing to do with the graph)
 
     public Node(int _v1_indx, int _weight, Node _next) {
         v1_indx = _v1_indx;
--- assignment_04/src/SP.java
+++ assignment_04/src/SP.java
@@ -1,11 +1,77 @@
+import org.w3c.dom.Node;
+
 class SP {
 	int INF = 1051;
 
+	// plain find minimum between two integers
 	public static int min(int a, int b) {
 		if (a <= b) {
 			return a;
 		}
 		return b;
+	}
+
+	// find the index of the minimal in an array given the indices of the elements
+	// to consider
+	public static int min_indx(int[] arr, int indx0, int indx1) {
+		if (arr[indx0] <= arr[indx1]) {
+			return indx0;
+		}
+		return indx1;
+	}
+
+	public static void swap(int[] arr, int indx0, int indx1) {
+		int tmp;
+		tmp = arr[indx0];
+		arr[indx0] = arr[indx1];
+		arr[indx1] = tmp;
+	}
+
+	// priority_queue: priority queue
+	// n: length of container (#)
+	// a: active elements in the priority queue (#)
+	// start: heapify start index
+	public static void heapify(int[] priority_queue, int n, int a, int start) {
+		int left_child_indx, right_child_indx, min_child_indx;
+		int tmp;
+
+		for (;;) {
+			left_child_indx = 2 * start + 1;
+			right_child_indx = 2 * start + 2;
+
+			// both children arent present
+			if (left_child_indx > a - 1) { // adjust a for indices
+				return;
+			}
+			// left child is present, right isnt
+			else if (right_child_indx > a - 1) { // adjust a for indices
+				// if left child is smaller, it should be higher - set swap target to left child
+				min_child_indx = left_child_indx;
+			}
+			// both children are present
+			else {
+				// find minimal child's indx
+				min_child_indx = min_indx(priority_queue, left_child_indx, right_child_indx);
+			}
+
+			// swap if the minimal is smaller than start
+			if (priority_queue[start] > priority_queue[min_child_indx]) {
+				swap(priority_queue, start, min_child_indx);
+
+				// set up to traverse further down
+				start = min_child_indx;
+			}
+			// if start was already minmal no changes need to be propagated, return
+			else {
+				return;
+			}
+		}
+	}
+
+	public static int init_priority_queue(int[] priority_queue, int n, int a) {
+		for (int i = a / 2 - 1; i >= 0; i--) {
+			heapify(priority_queue, n, a, i);
+		}
 	}
 
 	public static int getSPLen(int n, int m, int[] x, int[] y, int[] c, int s, int t) {
@@ -20,39 +86,46 @@ class SP {
 			Node next = graph[v0_indx].next;
 
 			// create new node and link to next
+			// CURRENTLY IT RECEIVES INPUT AS A DIRECTED GRAPH
+			// TODO: change input receival process
 			Node inserting = new Node(y[i], c[i], next);
 
 			// the new header is the one we just created
 			graph[v0_indx] = inserting;
 		}
 
-		// we will keep track of the set by setting a flag in this array
+		// we will keep track of the set by setting a flag in a boolean array
 		boolean[] shortest_path_found_set = new boolean[n];
 
-		int[] d = new int[n];
+		// pathfinding data storing arrays
+		int[] dist = new int[n];
 		int[] prev = new int[n];
 
 		// priority queue that is going to store (V \ S)'s vertices
-		int[] priority_queue = new int[n];
+		int priority_queue_size = n;
+		int[] priority_queue = new int[n]; // reflects d[]
+		int[] priority_queue_indx_map = new int[n]; // reflects the indices that map priority queue to d[]
+
+		// initialize priority queue
 
 		// initial dijkstra setup
-		// loop through the elements that we can visit from s
 		Node ref_node_s = graph[s];
-		Node iter;
 
 		// set all to inf fist
 		for (int i = 0; i < n; i++) {
-			d[i] = INF;
+			dist[i] = INF;
 		}
 
-		// starting vert's d is zero
-		d[s] = 0;
+		// starting vert's dist is zero
+		dist[s] = 0;
 
-		// iterate through edges outgoing from vert indx=s
+		Node iter;
 		iter = ref_node_s;
-		while (iter != null) {
-			d[iter.v1_indx] = iter.weight;
-			iter = iter.next;
+		int current_node_indx = s;
+
+		// main loop
+		while (priority_queue_size != 0) {
+			// choose x from unvisited vertices that minimizes dist[]
 		}
 
 		return -1;
